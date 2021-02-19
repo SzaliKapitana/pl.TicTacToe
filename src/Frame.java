b0f9d033549a5b51e49727c60b7f16339137d8eb
SzaliKapitana <rafal.szala03@gmail.com>
2021-02-19 18:11:56 +0100

    first commit

---
+++ src/Frame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+    private static final long serialVersionUID = 1L;
+    public Frame(){
+        super("Kółko i krzyżyk");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setSize(300,400);
+
+        setLocation(1000,100);
+
+        setVisible(true);
+
+    }
+    
+}
