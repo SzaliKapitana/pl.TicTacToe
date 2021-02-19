9ed9948a56393e801d713eb770cddb5d80aaff46
SzaliKapitana <rafal.szala03@gmail.com>
2021-02-19 18:51:49 +0100

    buttons

---
+++ src/Buttons.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Buttons extends JPanel {
+    private static final long serialVersionUID = 1L;
+    JButton reset;
+    public Buttons(){
+        reset = new JButton("RESET");
+        add(reset);
+    }
+
+   
+  
+
+    
+}
